16270962fc9c42384377a148afc884b1be8fafc6
dungnguyen <dungnqhe160727@fpt.edu.vn>
2021-12-13 20:51:03 +0700

    Update properly

--- live_demo.py
+++ live_demo.py
@@ -6,11 +6,13 @@ import numpy as np
 import os
 import time
 import pickle
-import matplotlib.pyplot as plt
-from preprocess_data import filter_col
+from preprocess_data import filter_col_live
+# import keras
+import winsound
 
-filename = 'final_model.sav'
-frame_to_keep = 150
+filename = 'svm_5shalf.sav'
+sound_path = 'mixkit-emergency-alert-alarm-1007.wav'
+frame_to_keep = 50
 font = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10, 400)
 fontScale = 1
@@ -19,7 +21,6 @@ lineType = 2
 path_pred_68 = os.path.join('facial-landmarks','shape_predictor_68_face_landmarks.dat')
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(path_pred_68)
-loaded_model = pickle.load(open(filename, 'rb'))
 
 
 landmark_pts_count = 68
@@ -29,20 +30,27 @@ for i in range(landmark_pts_count):
     features.append('px_'+str(i+1))
     features.append('py_'+str(i+1))
 
-def model(landmarks):
+def model(landmarks, lstm=False):
     landmarks = np.array(landmarks, dtype=np.float64)
     df = pd.DataFrame(landmarks, columns=features)
     df.replace(-1, np.NaN, inplace=True)
     df.interpolate(inplace=True, limit_direction='both')
-    df = filter_col(df)
+    df = filter_col_live(df)
     df = df.drop('mood', axis=1)
-    X = df.to_numpy().flatten().reshape(1, -1)
+    if lstm:
+        # loaded_model = keras.models.load_model("lstm_100.h5")
+        X = df.to_numpy()
+        X = np.reshape(X, (-1, frame_to_keep, X.shape[1]))
+    else:
+        loaded_model = pickle.load(open(filename, 'rb'))
+        X = df.to_numpy().flatten().reshape(1, -1)
     ypred = loaded_model.predict(X)
-    return ypred[0].astype(str)
+    return ypred[0]
 
 def live():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    data = np.full((100, 137), -1).tolist()
+    # data = np.full((frame_to_keep-30, 137), -1).tolist()
+    data = []
     result = []
     state = 'Caliberating...'
     start = 0
@@ -58,8 +66,8 @@ def live():
         #                 font, fontScale, fontColor, lineType)
 
         if len(data) > frame_to_keep-1:
-            # we only need last 149 frames
-            data = data[len(data)-149:]
+            # we only need last frame_to_keep-1 frames
+            data = data[len(data)-frame_to_keep+1:]
 
         interval = end - start
 
@@ -71,10 +79,10 @@ def live():
 
         # if no faces are found
         if len(rects) == 0:
-            data.append(['-1' for x in range(1+landmark_pts_count*2)])
+            data.append(['-1' for _ in range(1+landmark_pts_count*2)])
             # cv2.putText(image, 'Caliberating...', bottomLeftCornerOfText,
             #             font, fontScale, fontColor, lineType)
-        elif len(rects) >= 1 :
+        elif len(rects) >= 1:
             rect = rects[0]
 
             # Make the prediction and transfom it to numpy array
@@ -86,14 +94,15 @@ def live():
                 data.append(frame_data)
                 if len(data) == frame_to_keep:
                     mood = model(data)
-                    print(mood)
-                    if mood == '0':
-                        state = 'Alert'
+                    if mood == 0:
+                        state = 'Normal'
                     # elif mood == '5':
                     #     state = 'Normal'
                     else:
-                        state = 'Drowsy'
-                    # cv2.putText(image, state, bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
+                        winsound.PlaySound(sound_path, winsound.SND_ASYNC)
+                        state = 'DROWSINESS ALERT!'
+                        data = []
+                    print(state)
                     result.append(mood)
                     data = data[1:]
                     start = end
--- preprocess_data.py
+++ preprocess_data.py
@@ -5,34 +5,38 @@ import numpy as np
 import math
 
 face_points_to_keep = []
-face_points_to_keep += [9]                     # Nose
-face_points_to_keep += [37,38,39,40,41,42]     # Left Eye
-face_points_to_keep += [43,44,45,46,47,48]     # Right Eye
-face_points_to_keep += [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59] # Outer Lip
+face_points_to_keep += [9]  # Nose
+face_points_to_keep += [37, 38, 39, 40, 41, 42]  # Left Eye
+face_points_to_keep += [43, 44, 45, 46, 47, 48]  # Right Eye
+face_points_to_keep += [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]  # Outer Lip
 
-columns_to_keep = ['participant', 'mood', 'time'] + \
-                    [f'px_{x}' for x in face_points_to_keep] + \
-                    [f'py_{x}' for x in face_points_to_keep] +\
-                    ['face_x','face_y','face_w','face_h']
+columns_to_keep = ['mood'] + \
+                  [f'px_{x}' for x in face_points_to_keep] + \
+                  [f'py_{x}' for x in face_points_to_keep] + \
+                  ['face_x', 'face_y', 'face_w', 'face_h']
+
 
 def distance(table, a, b):
     return np.sqrt((table[f'px_{a}'] - table[f'px_{b}']) ** 2
                    + (table[f'py_{a}'] - table[f'py_{b}']) ** 2)
 
+
 def eye_aspect_ratio(table):
-	ear = (distance(table, 38, 42) + distance(table, 39, 41)) \
-                / (2 * distance(table, 37, 40))
-	return ear
+    ear = (distance(table, 38, 42) + distance(table, 39, 41)) \
+          / (2 * distance(table, 37, 40))
+    return ear
+
 
 def mouth_aspect_ratio(table):
     A = distance(table, 52, 58)
     C = distance(table, 49, 55)
-    mar = (A ) / (C)
+    mar = (A) / (C)
     return mar
+
 
 def circularity(table):
     A = distance(table, 38, 41)
-    radius  = A/2.0
+    radius = A / 2.0
     Area = math.pi * (radius ** 2)
     p = 0
     p += distance(table, 37, 38)
@@ -41,56 +45,65 @@ def circularity(table):
     p += distance(table, 40, 41)
     p += distance(table, 41, 42)
     p += distance(table, 42, 37)
-    return 4 * math.pi * Area /(p**2)
+    return 4 * math.pi * Area / (p ** 2)
+
 
 def mouth_over_eye(table):
-    ear = eye_aspect_ratio(table)
-    mar = mouth_aspect_ratio(table)
-    mouth_eye = mar/ear
+    mouth_eye = table['MAR'] / table['EAR']
     return mouth_eye
 
 
-def get_table(participant, mood, start_time=61, stop_time=361, resample_interval='100ms',
-              base_path=None):
+def get_table(index, base_path=None):
     # Find File
     if base_path is None:
-        base = os.path.join('output', 'csv')
+        base = 'CSV_DATA'
     else:
         base = base_path
 
-    files = glob.glob(os.path.join(base, f'{participant}_{mood}.csv'))
+    files = glob.glob(os.path.join(base, f'{index}.csv'))
 
     # Load
     table = pd.read_csv(files[0])
 
-    # Resample time
-    table['date'] = pd.to_datetime(table.time, unit='s')
-    if resample_interval is not None:
-        table = table.resample(resample_interval, on='date').mean()
-    else:
-        table.set_index('date', inplace=True)
-
     # Drop columns we don't need
     table = table.filter(columns_to_keep)
 
-    # Trim head and tail of the video
-    table.drop(table[table['time'] > stop_time].index, inplace=True)
-    table.drop(table[table['time'] < start_time].index, inplace=True)
+    table = table[::3]
 
     # Fill missing data
     table.replace(-1, np.NaN, inplace=True)
     table.interpolate(inplace=True, limit_direction='both')
 
     # Fix Data Types
-    table[['participant', 'mood']] = table[['participant', 'mood']].astype('int32')
+    table[['mood']] = table[['mood']].astype('int32')
 
     return table
 
 
 # process all data files
 def filter_col(table):
-    features = ['mood', 'EAR_N', 'MAR_N', 'PUC_N', 'MOE_N']
+    table['EAR'] = eye_aspect_ratio(table)
+    table['MAR'] = mouth_aspect_ratio(table)
+    table['PUC'] = circularity(table)
+    table['MOE'] = mouth_over_eye(table)
 
+    table['MOE'] = table['MOE'].replace([np.inf, -np.inf], np.nan)
+    table.interpolate(inplace=True, limit_direction='both')
+
+    features = ['EAR', 'MAR', 'PUC', 'MOE']
+    table_loc = table.iloc[:30, :]
+
+    for j in features:
+        table[f'{j}_mean'] = table_loc[f'{j}'].mean()
+        table[f'{j}_std'] = table_loc[f'{j}'].std()
+        table[f'{j}_N'] = (table[f'{j}'] - table[f'{j}_mean']) / table[f'{j}_std']
+
+    table = table.filter(['mood', 'EAR', 'MAR', 'PUC', 'MOE', 'EAR_N', 'MAR_N', 'PUC_N', 'MOE_N'])
+    return table
+
+
+# process all data files
+def filter_col_live(table):
     table['EAR'] = eye_aspect_ratio(table)
     table['MAR'] = mouth_aspect_ratio(table)
     table['PUC'] = circularity(table)
@@ -101,7 +114,7 @@ def filter_col(table):
     table["PUC_N"] = (table["PUC"] - table["PUC"].mean()) / table["PUC"].std()
     table["MOE_N"] = (table["MOE"] - table["MOE"].mean()) / table["MOE"].std()
 
-    #     table.filter(features)
-    return table[features]
+    table = table.filter(['mood', 'EAR', 'MAR', 'PUC', 'MOE', 'EAR_N', 'MAR_N', 'PUC_N', 'MOE_N'])
+    return table
 
 # print(get_table(31, 10))
